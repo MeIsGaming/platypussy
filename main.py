66003f1b03f1ea298f2fe283d158e5f9f053ce68
MeIsGaming <info@meisgaming.net>
2024-07-24 16:53:36 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ bot = commands.Bot(command_prefix=prefix)
 
 @bot.event
 async def on_ready():
-    print(f"AI Selfbot successfully logged in as {bot.user.name}.")
+    print(f"Successfully logged in as {bot.user.name}.")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    author = message.author
+    content = message.content
+    print(f"{author}: {content}")
+
+@bot.event
+async def on_member_update(before, after):
+    print(f"{before.nick} -> {after.nick}")
 
 bot.run(TOKEN)
