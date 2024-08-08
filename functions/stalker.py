dce7a097a7f1cbaa7330b9c8b5db13249fbae98d
MeIsGaming <info@meisgaming.net>
2024-08-09 00:17:28 +0200

    stalker.py works! UWU!

--- functions/stalker.py
+++ functions/stalker.py
@@ -3,36 +3,33 @@ import re
 
 EMOJI_REGEX = r'<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>'
 
-async def smessage(owner: discord.User, STALKUSERS, message: discord.Message):
-    if str(message.author.id) in STALKUSERS:
-        await  extr_emojis(message)
-        newmsg = message.replace(EMOJI_REGEX, )
+
+async def smessage(owner: discord.User, STALK_USERS, message: discord.Message):
+    if str(message.author.id) in STALK_USERS:
+        newmsg = replace_emojis_with_urls(message.content)
         await owner.send(
-            f"*{message.author.name}* wrote:\n```{newmsg}```\n-# in **[{message.channel.name}]({message.jump_url})** on  **{message.channel.guild.name}**")
+            f"*{message.author.name}* wrote:\n```{newmsg} ```\n-# in **[{message.channel.name}]({message.jump_url})** on  **{message.channel.guild.name}**")
 
 
-async def smessage_edit(owner: discord.User, STALKUSERS, before: discord.Message, after: discord.Message):
-    if str(before.author.id) in STALKUSERS:
-        await owner.send(f"*{before.author.name}* edited:\n```{before.content}```\nto\n```{after.content}```\n-# in **[{before.channel.name}]({after.jump_url})** on  **{before.channel.guild.name}**")
-
-
-async def smessage_delete(owner: discord.User, STALKUSERS, message: discord.Message):
-    if str(message.author.id) in STALKUSERS:
+async def smessage_edit(owner: discord.User, STALK_USERS, before: discord.Message, after: discord.Message):
+    if str(before.author.id) in STALK_USERS:
+        before_msg = replace_emojis_with_urls(before.content)
+        after_msg = replace_emojis_with_urls(after.content)
         await owner.send(
-            f"*{message.author.name}* deleted message:\n```{message.content}```\n-# in **[{message.channel.name}]({message.jump_url})** on  **{message.channel.guild.name}**")
+            f"*{before.author.name}* edited:\n```{before_msg} ```\nto\n```{after_msg} ```\n-# in **[{before.channel.name}]({after.jump_url})** on  **{before.channel.guild.name}**")
 
 
+async def smessage_delete(owner: discord.User, STALK_USERS, message: discord.Message):
+    if str(message.author.id) in STALK_USERS:
+        newmsg = replace_emojis_with_urls(message.content)
+        await owner.send(
+            f"*{message.author.name}* deleted message:\n```{newmsg} ```\n-# in **[{message.channel.name}]({message.jump_url})** on  **{message.channel.guild.name}**")
 
 
-async def extr_emojis(message):
-    emojis = re.findall(EMOJI_REGEX, message.content)
+def replace_emojis_with_urls(content: str) -> str:
+    def get_url(match):
+        animated, name, emoji_id = match.groups()
+        ext = "gif" if animated else "png"
+        return f" ``` [{name}](https://cdn.discordapp.com/emojis/{emoji_id}.{ext}?size=96&quality=lossless) ||{emoji_id}|| ``` "
 
-    for emoji in emojis:
-        get_url(emoji)
-
-    return emojis
-
-def get_url(emoji):
-    #name = emoji[1]
-    ext = "gif" if emoji[0] else "png"
-    return f"https://cdn.discordapp.com/emojis/{emoji[2]}.{ext}?size=96&quality=lossless"
+    return re.sub(EMOJI_REGEX, get_url, content)
